ccb4040c66756d69380b979ea4e85fc38b430493
IONIN66 <127221123+IONIN66@users.noreply.github.com>
2024-04-17 02:14:59 +0300

    fix

--- homeWork_04.js
+++ homeWork_04.js
@@ -96,3 +96,37 @@ const personThree = new PersonThree('Vova');
 
 // Ответ: 
 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const total = 13;
+
+// О(n^2) - квадратичная сложность
+
+function firstSum (arr, total){
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] + arr[j] === total) {
+          return [arr[i], arr[j]];
+        }
+      }
+    }
+    return [];
+};
+
+
+// О(n) - линейная сложность;
+
+function firstSum (arr, total){
+    let firstValue = 0;
+    let secondValue = arr.length - 1;
+  
+    while (firstValue < secondValue) {
+      const sum = arr[firstValue] + arr[secondValue];
+      if (sum === total) {
+        return [arr[firstValue], arr[secondValue]];
+      } else if (sum < total) {
+        firstValue++;
+      } else {
+        secondValue--;
+      }
+    }
+}
